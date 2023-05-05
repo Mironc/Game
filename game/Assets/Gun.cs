72295b19a9b90ff436175c2ddd79f270d44df6b5
Mironc <shafikovamirka@gmail.com>
2023-05-05 21:16:21 +0300

    1.0

--- game/Assets/Gun.cs
+++ game/Assets/Gun.cs
@@ -1,16 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using System;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+[CreateAssetMenu()]
+public class Gun : ScriptableObject
 {
-    [SerializeField]private GameObject Bullet;
-    [SerializeField]private Transform BulletPlace;
-    void Update()
+    public int MaxCountOfBullet;
+    public int CountOfBullets;
+    public float ReloadTime;
+    public ShootType TypeofShoot;
+    public Recoil TypeofRecoil;
+    public float DelayAboutShoot;
+    public float BulletSpeed;
+    public Vector2 RangeOfRandom;
+    public enum ShootType
     {
-        if(Input.GetMouseButtonDown(1))
-        {
-            Instantiate(Bullet,BulletPlace.transform.position,Quaternion.identity);
-        }
+        SemiAuto, 
+        Auto,
+        Single
     }
+    
+    public void DecreaseCountOfBullet()
+    {
+        CountOfBullets--;
+        CountOfBullets = Mathf.Clamp(this.CountOfBullets,0,this.MaxCountOfBullet);
+    }
+    
+[Serializable]
+public struct Recoil
+    {
+    public AnimationCurve RecoilDownPattern;
+    public float TimeOfShot;
+    public float RecoilPower;
+    public float RecoilInRun;
+    public float RecurrentSpeed;
+    public int SeedofRecoil;
+    public Vector2 RangeOfRandom;
+    public Vector2 CurrentRecoil;
+    public float ResetRecoilTimer;
+    public void SetRandom()
+    {
+        Random.InitState(this.SeedofRecoil);
+    }
+    public void AddRecoil()
+    {
+        if(this.TimeOfShot < this.RecoilDownPattern.keys[this.RecoilDownPattern.length].time )
+        /* CurrentRecoil = CharacterController.Instance.MovementProperties.Walk ? 
+        new Vector2(Random.Range(this.RangeOfRandom.x, this.RangeOfRandom.y), RecoilDownPattern.Evaluate(this.TimeOfShot)) * this.RecoilInRun * this.RecoilPower :
+        new Vector2(Random.Range(this.RangeOfRandom.x, this.RangeOfRandom.y), ) * this.RecoilPower;  */
+        this.CurrentRecoil = new Vector2(Random.Range(this.RangeOfRandom.x, this.RangeOfRandom.y), RecoilDownPattern.Evaluate(this.TimeOfShot))
+        * (CharacterController.Instance.MovementProperties.Walk ? this.RecoilInRun * this.RecoilPower : this.RecoilPower) ;
+        this.ResetRecoilTimer = 0;
+    }
+    public void RecoverRecoil() 
+    {
+        
+        CurrentRecoil = Vector2.Lerp(CurrentRecoil, Vector2.zero, this.ResetRecoilTimer);
+        this.ResetRecoilTimer += Time.deltaTime * this.RecurrentSpeed;
+    }
+    }    
 }
